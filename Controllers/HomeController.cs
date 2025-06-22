1f812ca97abd7b481aa1dc0359cb8abe736b3017
u24623815 <u24623815@tuks.co.za>
2025-06-22 05:46:32 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Homework.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SelectService()
+        {
+            return View();
+        }
+
+        public ActionResult BookingForm(string serviceType)
+        {
+            ViewBag.ServiceType = serviceType;
+            return View();
+        }
+
+        public ActionResult BookingConfirmed()
+        {
+            return View();
+        }
+
+        public ActionResult RideHistory()
+        {
+            return View();
+        }
+
+        public ActionResult DriverVehicleManagement()
+        {
+            return View();
+        }
+
+
+
+    }
+}
